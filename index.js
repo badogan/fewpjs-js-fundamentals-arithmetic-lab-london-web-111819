3fc205890d2ddfc05cfbe99e4472e935ddb51cf0
badogan <basri.dogan.71@gmail.com>
2020-01-10 22:22:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+function multiply(a,b){
+    return (a*b)
+}
+
+function random(){
+    let bool = true 
+    while (bool) {
+    let returnValue = Math.random()
+    if (returnValue > 0){
+        return returnValue;
+    }
+    }
+}
+
