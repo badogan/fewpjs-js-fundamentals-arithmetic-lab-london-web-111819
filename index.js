416b0e8c5a825bcf1ec6c1a69932f786789981dc
badogan <basri.dogan.71@gmail.com>
2020-01-10 22:25:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 
 // Write your code here
 
-function multiply(a,b){
-    return (a*b)
-}
-
-function random(){
-    let bool = true 
-    while (bool) {
-    let returnValue = Math.random()
-    if (returnValue > 0){
-        return returnValue;
-    }
-    }
-}
-
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor( Math.random() * 1000 ) + 1
+const mod = 16 % 6
+const max = Math.max( 5, 10, 15, 20 )
